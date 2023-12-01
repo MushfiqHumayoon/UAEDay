6080cbfb1bb2cb6605d963b8857da98a408aa898
Mushfiq Humayoon <mushfiqhumayoon@gmail.com>
2023-12-01 16:37:20 +0400

    Initial Commit

---
+++ UAEDay/UAEDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  UAEDayApp.swift
+//  UAEDay
+//
+//  Created by Mushfiq Humayoon on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UAEDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
